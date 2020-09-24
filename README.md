32423661ec787c7befe8522670e145bb3851f927
Asish Binu Mathew <asishbinumathew2020@it.ajce.in>
2020-09-24 14:34:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# JupyterLauncher
+Jupyter Launcher 
+
+Instead of opening terminal and running jupyter notebook a one click exe to run Jupyter Notebook in a default directory.
---
+++ launcher.bat
@@ -0,0 +1,35 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFBpHRReKAE+1BaAR7ebv/NawrV8WUu4wb7PZ26eHI9w5/1X9YZM/+ntPlsgECQlkdxGkUgIhpXpLiW2GOMnSugzuKg==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBpHRReKAE+/Fb4I5/jH5eaGt0MeXOctNorD39Q=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+cd C:\Notebooks
+jupyter notebook
